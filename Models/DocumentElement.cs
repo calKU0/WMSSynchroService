3e2355f8a09a544317c59b2ebcbd439c17836883
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2025-01-27 11:49:25 +0100

    add new class

--- Models/DocumentElement.cs
+++ Models/DocumentElement.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PinquarkWMSSynchro.Models
+﻿namespace PinquarkWMSSynchro.Models
 {
     public class DocumentElement
     {
         public int ErpId { get; set; }
-        public int Quantity { get; set; }
-        public int No { get; set; }
-        public string StatusSymbol { get; set; }
+        public string Source { get; set; }
+        public string Unit { get; set; }
     }
 }
